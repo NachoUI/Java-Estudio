310a98ea46031ed480059f1f4fec2fad3d3a1209
NachoUI <ignacio09code@gmail.com>
2025-04-14 19:42:58 -0400

    Se hace la practica que se pidio

---
+++ PracticaUno/MiInformacion.java
@@ -0,0 +1,16 @@
+class MiInformacion {
+    public static void main(String[] args) {
+        String nombre = "Ignacio Munoz";
+        int edad = 24;
+        String pais = "Chile";
+        String ciudad = "Santiago";
+        String hobbie_uno = "Entrenar";
+        String hobbie_dos = "Meditar";
+
+        System.err.println("Hola, mi nombre es " + nombre);
+        System.err.println("Tengo " + edad + " años de edad");
+        System.err.println("Soy de " + pais + " vivo en la ciudad de "  + ciudad);
+        System.err.println("Mis hobbies son: " + hobbie_uno + "y" + hobbie_dos);
+        
+    }
+}
